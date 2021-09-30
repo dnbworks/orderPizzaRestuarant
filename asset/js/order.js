87f1cf20a9323bd70cdee1132b714aaa94e9dcf3
dnbworks <dooyongnsaakobalderama@gmail.com>
2021-09-30 14:19:46 +0800

    updated json

--- asset/js/order.js
+++ asset/js/order.js
@@ -56,7 +56,7 @@ lis.forEach(li => {
 
 
 const getMenu = async () => {
-    const response = await fetch('http://127.0.0.1:5500/data.json');
+    const response = await fetch('https://dnbworks.github.io/orderPizzaRestuarant/data.json');
     const data = await response.json();
     return data;
 };
